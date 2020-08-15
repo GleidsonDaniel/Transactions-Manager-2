edd3e69cea9a9933290ccb4a374369c7ccefb1e5
Daniel <gleidson10daniel@hotmail.com>
2020-08-15 10:50:51 -0300

    Checks the user's status and sends it to the specified route

--- src/screens/Preload/index.tsx
+++ src/screens/Preload/index.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useAsyncStorage} from '@react-native-community/async-storage';
+import {useNavigation} from '@react-navigation/native';
 
 import {logo} from '@/assets';
 
 import {Container, AppName, Logo} from './styles';
+import {typeRoutes} from '@/routes/types';
 
 const Preload: React.FC = () => {
+  const navigation = useNavigation();
+  const {getItem} = useAsyncStorage('@logged');
+
+  const handleUserStatus = async () => {
+    const logged = await getItem();
+    // simulating loading the API to verify user data
+    setTimeout(() => {
+      if (!!logged) {
+        return navigation.navigate(typeRoutes.home);
+      }
+      return navigation.navigate(typeRoutes.initial);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    handleUserStatus();
+  }, []);
+
   return (
     <Container>
       <Logo source={logo} resizeMode="contain" />
